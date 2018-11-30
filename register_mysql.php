72222f65d1a1750ab595f6943bfbcb6f1b5367f0
chaise allegra <chaise.allegra@gmail.com>
2018-11-29 18:12:25 -0800

    front end changes to php and game pages

--- register_mysql.php
+++ register_mysql.php
@@ -90,11 +90,30 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Sign Up</title>
+    <link rel="stylesheet" href="styleGame.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Aldrich|Bungee+Inline|Fredericka+the+Great|Kumar+One+Outline|Monofett|Monoton|Notable|Orbitron|Patua+One|Press+Start+2P|Questrial|Righteous|Teko"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather|Noto+Serif" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond|Eczar|Gentium+Basic|Libre+Baskerville|Libre+Franklin|Proza+Libre|Rubik|Taviraj|Trirong|Work+Sans"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Bungee+Shade" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Ewert|Gravitas+One|Monoton" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond|Eczar|Gentium+Basic|Libre+Baskerville|Libre+Franklin|Proza+Libre|Rubik|Taviraj|Trirong|Work+Sans"
+        rel="stylesheet">
+    <script type="text/javascript" src="logic.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Merriweather|Noto+Serif" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond|Eczar|Gentium+Basic|Libre+Baskerville|Libre+Franklin|Proza+Libre|Rubik|Taviraj|Trirong|Work+Sans"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Ewert|Gravitas+One|Monoton" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Aldrich" rel="stylesheet">
 </head>
 <body>
-    <div>
-        <h2>Sign Up</h2>
-        <p>Please fill this form to create an account.</p>
+ 
+    <section id="register">
+        <div id="registerDiv">
+        <article>Sign up</article>
+        <hr>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>Username:<sup>*</sup></label>
@@ -103,20 +122,22 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>    
             <div <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
                 <label>Password:<sup>*</sup></label>
-                <input type="password" name="password" value="<?php echo $password; ?>">
+                <input style="margin-left:1.5%;"type="password" name="password" value="<?php echo $password; ?>">
                 <span><?php echo $password_err; ?></span>
             </div>
             <div <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-                <label>Confirm Password:<sup>*</sup></label>
-                <input type="password" name="confirm_password" value="<?php echo $confirm_password; ?>">
+                <label>Confirm:<sup>*</sup></label>
+                <input style="margin-left:6%;"type="password" name="confirm_password" value="<?php echo $confirm_password; ?>">
                 <span><?php echo $confirm_password_err; ?></span>
             </div>
-            <div>
+            <div id="registerButtons">
                 <input type="submit" value="Submit">
                 <input type="reset"  value="Reset">
             </div>
-            <p>Already have an account? <a href="login_mysql.php">Login here</a>.</p>
+            <div id="helpDiv">Already have an account? <a href="login_mysql.php">Login here</a>.</div>
         </form>
-    </div>    
+        </section>
+    </div>
+    
 </body>
 </html>
